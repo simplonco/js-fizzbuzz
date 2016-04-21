3f1e05cb47545953ec8b2247d5d46b5a3a05a824
aboualnaser <aboualnaser@gmail.com>
2016-04-21 14:01:10 +0200

    first commit

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -4,10 +4,24 @@
 // Make sure to look at test.script.js--that should give you some hints about what is
 // expected here.
 
-var fizzbuzz = function ()  {
-//
-// YOUR CODE GOES HERE
-//
+var fizzbuzz = function(x) {
+    if (x%3==0) {
+return "Fizz";
+    }
+    if
+    else (x%5==0){
+return "Buzz" ;
+    }
+
+    if
+    else (x%3 ==0&&x%5==0)
+{
+  return "FizzBuzz";
+}
+    else {
+      return "x"
+    }
+
 };
 
 module.exports = fizzbuzz;
