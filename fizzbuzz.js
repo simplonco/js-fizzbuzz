26dfd032454bab45afdee2a225ba82006d58cf0d
adora84 <abdulkader.kalaaji@gmail.com>
2016-04-21 12:15:15 +0200

    third try

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -5,9 +5,15 @@
 // expected here.
 
 var fizzbuzz = function(input) {
-    if (input % 3 === 0 ){ console.log(fizz);}
-      else if (input % 5 === 0){console.log(buzz);}
-        else if (input % 3 == 0 || input % === 0 ){console.log(fizzbuzz);}
-}else { console.log(input);}
+    if (input % 3 === 0) {
+        console.log("fizz");
+    } else if (input % 5 === 0) {
+        console.log("buzz");
+    } else if (input % 3 == 0 || input % 5 === 0) {
+        console.log("fizzbuzz");
+    } else {
+        console.log(input);
+    }
+}
 
-module.exports = fizzbuzz;
+fizzbuzz(5);
