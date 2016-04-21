7d475431f792f2b909752364fd8679346da3911b
thamoui <hamoui.antoine@gmail.com>
2016-04-21 11:05:14 +0200

    test1

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -5,9 +5,20 @@
 // expected here.
 
 var fizzbuzz = function ()  {
-//
-// YOUR CODE GOES HERE
-//
+  fizzbuzz.prototype.hello = function(num) {
+    if (num % 3 ==1){
+      return "Fizz";
+    }else if (num % 5 == 1) {
+      return "Buzz";
+
+    }else if ((num%3 && num%5) == 1) {
+      return FizzBuzz;
+    }
+
+    return "Hiiii";
+  };
+
+
 };
 
 module.exports = fizzbuzz;
