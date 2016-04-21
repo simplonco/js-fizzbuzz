0ffdd47864cf08e45a49c1c3d932f89cbbb7bfc9
amare <amare>
2016-04-21 14:14:59 +0200

    amare's first exercise on js

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -4,10 +4,17 @@
 // Make sure to look at test.script.js--that should give you some hints about what is
 // expected here.
 
-var fizzbuzz = function ()  {
-//
-// YOUR CODE GOES HERE
-//
+var fizzbuzz = function (number)  {
+  if (number % 3 == 0) {
+    console.log("number is a multiple of 3");
+  }
+  if (number % 5 == 0) {
+    console.log("number is a multiple of 5");
+  }
+  if ((number % 3 == 0) && (number % 5 == 0)) {
+    console.log("number is a multiple of 3 and 5");
+  }
 };
+fizzbuzz(30);
 
-module.exports = fizzbuzz;
+//module.exports = fizzbuzz;
