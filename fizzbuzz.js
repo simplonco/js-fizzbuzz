5e6c076bf4000dc216b326db6ae12aef1a3d02c8
thamoui <hamoui.antoine@gmail.com>
2016-04-21 11:16:50 +0200

    test3

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -4,19 +4,19 @@
 // Make sure to look at test.script.js--that should give you some hints about what is
 // expected here.
 
-var fizzbuzz = function ()  {
-  fizzbuzz.prototype.hello = function(num) {
-    if ((num % 3) ===1){
+var fizzbuzz = function (num)  {
+
+    if ((num % 3) ==1){
       return "Fizz";
     }
-    if ((num % 5) === 1) {
+    if ((num % 5) == 1) {
       return "Buzz";
 
     }
-    if ((num%3 && num%5) === 1) {
+    if ((num%3 && num%5) == 1) {
       return FizzBuzz;
     }
-  };
+
 
 
 };
