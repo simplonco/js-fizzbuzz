908fbc53f1dcfcc56452ea6dc4eee19fa3e3588c
thamoui <hamoui.antoine@gmail.com>
2016-04-21 11:42:11 +0200

    test4

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -5,16 +5,16 @@
 // expected here.
 
 var fizzbuzz = function (x)  {
-   if ((x%2) ==1){
+   if ((x%2) ===1){
      return "2";
    }
-   if ((x%3) ==1){
+   if ((x%3) ===1){
      return "Fizz";
    }
-   if ((x%5) ==1){
+   if ((x%5) ===1){
      return "Buzz";
    }
-   if ((x%3)&&(x%5) ==1){
+   if ((x%3)&&(x%5) ===1){
      return "FizzBuzz";
    }
 };
