f7a20a66df97c80257735d9b04a3875809ac278a
syed zeeshan ali zaidi <zeshanalizaidi@gmail.com>
2016-04-21 13:26:48 +0200

    First solution

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -4,10 +4,22 @@
 // Make sure to look at test.script.js--that should give you some hints about what is
 // expected here.
 
-var fizzbuzz = function (x)  {
-//
-// YOUR CODE GOES HERE
-//
-};
+/*
+ * print out "Fizz" if the integer is a multiple of 3 (and not a multiple of 5)
+ * print out "Buzz" if the integer is a multiple of 5 (and not a multiple of 3)
+ * print "FizzBuzz" if the integer is a multiple of 3 and 5
+ * print the number if the integer is not a multiple of 3 or 5
+ */
+
+var fizzbuzz = function (i)  {
+    if (i % 3 == 0 && i % 5 == 0)
+        console.log("FizzBuzz");
+    else if (i % 3 == 0)
+        console.log("Fizz");
+    else if (i % 5 == 0)
+        console.log("Buzz");
+    else
+        console.log(i);
+}
 
 module.exports = fizzbuzz;
