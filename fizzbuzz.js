34f4782644025c8eae47de8d129b78c1d396eb0f
simplonco <saba.chadi.michel.com>
2016-04-22 11:31:58 +0200

    chadi

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -4,10 +4,23 @@
 // Make sure to look at test.script.js--that should give you some hints about what is
 // expected here.
 
-var fizzbuzz = function (x)  {
-//
-// YOUR CODE GOES HERE
-//
+var fizzbuzz = function (num)  {
+ var result;
+    if (((num % 2 )=== 0) && (num !== 6) && (num !== 30)){
+     return result= 2; 
+ }else if((num %  5)=== 0)
+ {
+    return result = "buzz" ;
+ }else if (((num % 6 )=== 0)&&(num >= 6)){
+     return result = "bizz";
+ }else if ((num % 11 )=== 0){
+     return result = 11 ; 
+ }else if( ((num % 30 )=== 0)&&(num >= 30)){
+     return result = "fizzbuzz" ; 
+ }else{
+      return result= "";
+ }
+ alert(result) ;
 };
 
 module.exports = fizzbuzz;
