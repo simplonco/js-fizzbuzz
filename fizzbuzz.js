e0f7037a2d78a8004bddf8649c2aed8d6dbc863a
Chadi Saba <saba.chadi.michel@gmail.com>
2016-04-25 16:47:49 +0200

    final2

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -7,17 +7,16 @@
 'use strict';
 
 var fizzbuzz = function (x) {
-  if (((x % 3) == 0) && ((x % 5) == 0)) {
+  if (x % 15 === 0) {
         return "fizzbuzz";
 
-    } else if (((x % 3) == 0) && ((x % 5) !== 0))
+    } else if (x % 5 ===0)
 
     {
         return "fizz";
 
-    } else if (((x % 3) !== 0) && ((x % 5) == 0))
-
-    {
+    } else if (x % 3 === 0)
+      {
         return "buzz";
 
     } else {
