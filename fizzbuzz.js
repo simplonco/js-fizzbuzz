585f5ba775a0d3e38fa56bd9ad50db32ba1c1c0a
Chadi Saba <saba.chadi.michel@gmail.com>
2016-04-25 13:12:36 +0200

    25-3 fizz

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -7,17 +7,22 @@
 'use strict';
 
 var fizzbuzz = function (x) {
- if ((x % 3 == 0)&&(x % 5 == 0)){
-    return "fizzbuzz" ; 
-  }else if ((x % 3 == 0)&&(x % 5 != 0))
-    
-  {  return "fizz";
-  }else if ((x % 3 !== 0)&&(x % 5 == 0))
-   
-   {  return "buzz";
-  }else{
-    return x;
-  } 
+  if (((x % 3) == 0) && ((x % 5) == 0)) {
+        return "fizzbuzz";
+
+    } else if (((x % 3) == 0) && ((x % 5) !== 0))
+
+    {
+        return "fizz";
+
+    } else if (((x % 3) !== 0) && ((x % 5) == 0))
+
+    {
+        return "buzz";
+
+    } else {
+        return x;
+    }
 };
 
 module.exports = fizzbuzz;
