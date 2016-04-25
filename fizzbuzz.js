e6e520d6cf77a44fc36b989f4f99795640eaad04
aboualnaser <aboualnaser@gmail.com>
2016-04-25 17:05:03 +0200

    new

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -5,15 +5,18 @@
 // expected here.
 
 var fizzbuzz = function(x) {
-    if (x % 3 == 0) {
-        return "fizz";
-    }
-    else if (x % 5 == 0) {
+  if (x % 3 === 0 || x % 5 === 0) {
+  if (x % 3 === 0 && x % 5 === 0) {
+      return "fizzbuzz";
+  }
+
+    else if (x % 5 === 0) {
         return "buzz";
     }
-    else if (x % 3 == 0 && x % 5 == 0) {
-        return "fizzbuzz";
+    else   {
+        return "fizz";
     }
+  }
     else {
         return x;
     }
