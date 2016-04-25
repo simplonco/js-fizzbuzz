e740c23a9786319f215b3140e495c1edfd1dd4a4
Adam Sakhr <adam.sakhr@gmail.com>
2016-04-25 17:23:19 +0200

    Hope is last

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -4,6 +4,7 @@
 // Make sure to look at test.script.js--that should give you some hints about what is
 // expected here.
 
+'use strict';
 
 var fizzbuzz = function(n) {
         if (n % 15 == 0) {
@@ -14,19 +15,9 @@ var fizzbuzz = function(n) {
             return 'buzz';
         } else {
             return n;
-        } 
+        }
         var fizzbuzz = function(x) {
 
         };
-
-        module.exports = fizzbuzz;
-
-'use strict';
-
-var fizzbuzz = function (x) {
-//
-// YOUR CODE GOES HERE
-//
-};
 
 module.exports = { fizzbuzz: fizzbuzz };
