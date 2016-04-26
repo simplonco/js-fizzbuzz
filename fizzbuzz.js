268916a3712ed4f86578abc5a57356445ac53d25
bmiande <mbaimiande@gmail.com>
2016-04-26 12:11:26 +0200

    Update fizzbuzz.js

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -7,9 +7,17 @@
 'use strict';
 
 var fizzbuzz = function (x) {
-//
-// YOUR CODE GOES HERE
-//
+if((x % 3 === 0)) && ((x % 5 === 0))
+console.log("FizzBuzz");
+}
+else if(x % 3 === 0){
+  console.log ("Fizz");
+}
+else if(x % 5 === 0){
+  console.log("Buzz");
+}
+else{
+  console.log(x);
 };
-
+};
 module.exports = { fizzbuzz: fizzbuzz };
