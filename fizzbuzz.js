76ffa788e8332baf4218f0543fce57dc32e0f52a
adora84 <abdulkader.kalaaji@gmail.com>
2016-04-27 14:19:09 +0200

    test1

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -12,7 +12,7 @@ var fizzbuzz = function(x) {
     //
     if (x % 3 === 0 && x % 5 !== 0) {
         return "fizz";
-    } else if (x % 5 === 0 && x % 5 !== 0) {
+    } else if (x % 5 === 0 && x % 3 !== 0) {
         return "buzz";
     } else if (x % 3 === 0 || x % 5 === 0) {
         return "fizzbuzz";
