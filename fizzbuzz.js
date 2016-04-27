a6475df174ea3eb70c457c9ddd37a82cd13d0d03
mardi yahia <mardi.yahia@gmail.com>
2016-04-27 11:39:05 +0200

    third check

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,31 +1,26 @@
+
 //
 // This is only a SKELETON file for the 'FizzBuzz' exercise. It's been provided as a
 // convenience to get you started writing code faster.
 // Make sure to look at test.script.js--that should give you some hints about what is
 // expected here.
 
-
-var fizzbuzz = function (c)  {
- if(c % 3 === 0 && c % 5 === 0){
-   return "fizzbuzz";
- }
- else if(c % 3 === 0) {
-   return "fizz";
- }
- else if(c % 5 === 0){
-   return "buzz";
- }
- else {
-   return c;
- }
-
 'use strict';
 
 var fizzbuzz = function (x) {
-//
-// YOUR CODE GOES HERE
-//
-
+  var fizzbuzz = function (c)  {
+   if(c % 3 === 0 && c % 5 === 0){
+     return "fizzbuzz";
+   }
+   else if(c % 3 === 0) {
+     return "fizz";
+   }
+   else if(c % 5 === 0){
+     return "buzz";
+   }
+   else {
+     return c;
+   }
 };
 
 module.exports = { fizzbuzz: fizzbuzz };
