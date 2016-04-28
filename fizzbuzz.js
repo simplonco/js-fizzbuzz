da10cd69b80e4b8edce12642b05982c0ed3be671
bmiande <mbaimiande@gmail.com>
2016-04-28 10:23:27 +0200

    Update fizzbuzz.js

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -7,16 +7,14 @@
 'use strict';
 
 var fizzbuzz = function (x) {
-}else if(x % 5===0)
-{
-  return"fizz";
-  return"buzz";
-}else if(x % 3===0)
-{
-  return"buzz";
-  return"fizz";
-}else{
-  return x;
-};
+if (x % 15 === 0) {
+            return "fizzbuzz";
+            } else if (x % 5 === 0) {
+              return "buzz";
+          } else if (x % 3 === 0) {
+              return "fizz";
+          } else {
+              return x;
+      }
 };
 module.exports = { fizzbuzz: fizzbuzz };
