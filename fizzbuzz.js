f61c211a2f62e180bb27566c5d9bfd5d4852b079
root <benoitvpc@gmail.com>
2016-10-12 13:05:12 +0200

    Test 3

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -8,7 +8,13 @@
 
 var fizzbuzz = function (x) {
 //
-//test2
+if (theNumber is divisible by 3) then
+print "Fizz"
+else if (theNumber is divisible by 5) then
+print "Buzz"
+else /* theNumber is not divisible by 3 or 5 */
+print theNumber
+end if
 //
 };
 
