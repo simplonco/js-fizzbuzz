652ea94441c3aa231558a9c205fcddd1c4dfcd38
WilfriedBlue <40619550+WilfriedBlue@users.noreply.github.com>
2018-06-28 12:24:10 +0200

    AHHHHHHHHHHHHHH

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -7,14 +7,18 @@
 'use strict';
 
 var fizzbuzz = function (x) {
-    if (x % 15 == 0)
-        console.log("FizzBuzz");
-    else if (x % 3 == 0)
-        console.log("Fizz");
-    else if (x % 5 == 0)
-        console.log("Buzz");
-    else
-        console.log(x);
+    
+    if ( x % 15 == 0) {
+        return "FizzBuzz";
+        
+    else if (x % 3 == 0) {
+        return "Fizz";
+        
+    else if (x % 5 == 0) {
+        return "Buzz";
+        
+    else {
+        return x;
   
 };
 
