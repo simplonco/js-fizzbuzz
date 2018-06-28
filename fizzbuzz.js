d2f51611c635b0da4423b9baaa9e8287a2dd3c55
Yverick <40633415+Yverick@users.noreply.github.com>
2018-06-28 12:13:46 +0200

    Update fizzbuzz.js

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -7,17 +7,18 @@
 'use strict';
 
 var fizzbuzz = function (x) {
-if ( x % 15 == 0) {
-    return "fizzbuzz";
+    if ( x % 15 == 0) {
+        return "fizzbuzz";
   }
-  else if (x % 3 == 0 && x % 5 != 0) {
-    return "fizz";
+    else if (x % 3 == 0 && x % 5 != 0) {
+        return "fizz";
   }
-  else if (x % 5 == 0 && x % 3 != 0) {
-    return "buzz";
+    else if (x % 5 == 0 && x % 3 != 0) {
+        return "buzz";
   }
-  else {
-    return x;
+    else {
+        return x;
+    }
 };
 
 module.exports = { fizzbuzz: fizzbuzz };
