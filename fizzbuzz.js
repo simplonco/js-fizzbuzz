48be268b539cc4c18f23d81fe488ecebcab2e104
WilfriedBlue <40619550+WilfriedBlue@users.noreply.github.com>
2018-06-28 12:00:04 +0200

    Update

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -7,9 +7,15 @@
 'use strict';
 
 var fizzbuzz = function (x) {
-//
-// YOUR CODE GOES HERE
-//
+    if (x % 15 == 0)
+        console.log("FizzBuzz");
+    else if (x % 3 == 0)
+        console.log("Fizz");
+    else if (x % 5 == 0)
+        console.log("Buzz");
+    else
+        console.log(x);
+  
 };
 
 module.exports = { fizzbuzz: fizzbuzz };
