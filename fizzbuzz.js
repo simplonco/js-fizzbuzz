19dc8b60c78268726362c92f185d150d90dd7553
seguyot <40633423+seguyot@users.noreply.github.com>
2018-06-28 11:54:55 +0200

    update nz

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -7,15 +7,13 @@
 'use strict';
 
 var fizzbuzz = function (x) {
-  if (x % 15 === 0) {
-        return "fizzbuzz";
-    } else if (x % 5 === 0) {
-        return "buzz";
-    } else if (x % 3 === 0) {
-        return "fizz";
+if (x % 15 === 0) {
+    return "fizzbuzz";
+  }else if (x % 5 === 0) {
+    return "buzz";
+  } else if (x % 3 === 0) {
     } else {
-        return x;
-    }
-};
-
+    return x;
+  }
+  };
 module.exports = { fizzbuzz: fizzbuzz };
