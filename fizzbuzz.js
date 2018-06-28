89fba682fe72e8ff579d73a91d27e0b0db8e0358
WilfriedBlue <40619550+WilfriedBlue@users.noreply.github.com>
2018-06-28 12:29:53 +0200

    J'ai FAIM !

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -10,16 +10,16 @@ var fizzbuzz = function (x) {
     
     if ( x % 15 == 0) {
         return "FizzBuzz";
-        
+    }
     else if (x % 3 == 0) {
         return "Fizz";
-        
+    }
     else if (x % 5 == 0) {
         return "Buzz";
-        
+    }  
     else {
         return x;
-  
+    };
 };
 
 module.exports = { fizzbuzz: fizzbuzz };
