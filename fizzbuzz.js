bf8d91c9f2a1cf683b7d9aa8f4d487dffa3136a1
Maureen-Depresle <mau.dep22@outlook.fr>
2020-04-06 15:35:51 +0200

    fizzbuzz maureen test2

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -8,17 +8,15 @@
 
 var fizzbuzz = function (x) {
 //
-    for (let i = 1; i <= 100; i++) {
-        if (i % 3 == 0 && i % 5 == 0) {
-            console.log("FizzBuzz");
-        } else if (i % 3 == 0) {
-            console.log("Fizz");
-        } else if (i % 5 == 0) {
-            console.log("Buzz");
+        if (x % 3 === 0 && x % 5 !== 0) {
+            console.log("fizz");
+        } else if (x % 5 === 0 && x % 3 !== 0) {
+            console.log("buzz");
+        } else if (x % 5 === 0) {
+            console.log("fizzbuzz");
         } else {
-            console.log(i);
-        }
-    }// YOUR CODE GOES HERE
+            console.log(x);
+        }// YOUR CODE GOES HERE
 //
 };
 
