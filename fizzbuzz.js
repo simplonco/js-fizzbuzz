d84122769e52872d863c6f7d68d0e4a6020b9eca
Clément Debatisse <clementdebatisse1996@gmail.com>
2020-04-06 15:46:55 +0200

    1er essai fizzbuzz CD

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -7,9 +7,21 @@
 'use strict';
 
 var fizzbuzz = function (x) {
-//
-// YOUR CODE GOES HERE
-//
-};
-
-module.exports = { fizzbuzz: fizzbuzz };
+    for(var i= 1; i <= 100; i++){
+        console.log(i);
+        if(i % 5 ===0; i % 3 ===0){
+            console.log("fizzbuzz")
+        }
+        else if(i % 3 !==0; i % 5 !==0){
+            console.log(i)
+        }
+        else if(i % 3 ===0; i % 5 !== 0){
+            console.log("fizz")
+        }
+        else if(i % 5 ===0; i % 3 !==0){
+            console.log("buzz");
+        }
+    }
+    
+    module.exports = { fizzbuzz: fizzbuzz };
+    
