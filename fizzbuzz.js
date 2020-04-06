4483d169b1f29079ada1f123118b8dc600f6330c
Clément Debatisse <clementdebatisse1996@gmail.com>
2020-04-06 16:53:16 +0200

    je passe à la télé

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -10,16 +10,15 @@ var fizzbuzz = function (x) {
     if (x%5 ===0 && x%3 ===0){
         return "fizzbuzz";
     }
-    else if (x%3 ===0 && x %5 !==0){
+    else if (x%3 ===0){
         return "fizz";
     }
-    else if (x%5 ===0 && x%3 !==0){
+    else if (x%5 ===0){
         return "buzz";
     }
     else {
         return x
     }
-}
 };
 
 module.exports = { fizzbuzz: fizzbuzz };
