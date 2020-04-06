40183096d0e882134ccc6e17d21b7fb83381e1d7
usher kadio <kadev0502@gmail.com>
2020-04-06 10:47:30 +0200

    modif du fichier js kadio usher

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -4,12 +4,28 @@
 // Make sure to look at test.script.js--that should give you some hints about what is
 // expected here.
 
-'use strict';
+"use strict";
 
-var fizzbuzz = function (x) {
-//
-// YOUR CODE GOES HERE
-//
+var fizzbuzz = function(x) {
+	// declaration de la boucle( init nombre, condition de la boucle et itération)
+	for (let nombre = 0; nombre <= 50; nombre++) {
+		// condition "fizzbuzz" si nombre multiple de 3 et 5
+		if (nombre % 3 === 0 && nombre % 5 === 0) {
+			console.log("fizzbuzz");
+
+			// condition "fizz" si nombre multipe de
+		} else if (nombre % 3 === 0) {
+			console.log("fizz");
+
+			// condition "buzz" si nombre multiple de 5
+		} else if (nombre % 5 === 0) {
+			console.log("buzz");
+
+			//affiche nombre si pas multiple de 5 ni de 5
+		} else {
+			console.log(nombre);
+		}
+	}
 };
 
 module.exports = { fizzbuzz: fizzbuzz };
