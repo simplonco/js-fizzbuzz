176812e928d90551d5c8138f6e882c4d847fce9c
Esther-Itam <esther.itam@live.fr>
2020-04-06 15:46:38 +0200

    modif

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -7,12 +7,12 @@
 'use strict';
 
 var fizzbuzz = function (x) {
-  if (x%3===0){
+  if (x%3===0) {
   return "fizz";}
-  elseif (x%5===0){
-   return "buzz"}
-  elseif (x%3===0 && x%5===0){
-    return "fizzbuzz"}
+  else if (x%5===0){
+   return "buzz";}
+  else if (x%3===0 && x%5===0){
+    return "fizzbuzz";}
   else{ return x;}
 };
 
