f4761d6e85486317649b310c3aeacf8980f68f08
Clément Debatisse <clementdebatisse1996@gmail.com>
2020-04-06 16:30:55 +0200

    jamais 2...

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -7,21 +7,19 @@
 'use strict';
 
 var fizzbuzz = function (x) {
-    for(var x= 1; x <= 100; x++){
-        console.log(x);
-        if(x % 5 ===0; x % 3 ===0){
-            console.log("fizzbuzz")
-        }
-        else if(x % 3 ===0; x % 5 !== 0){
-            console.log("fizz")
-        }
-        else if(x % 5 ===0; x % 3 !==0){
-            console.log("buzz");
-        }
-        else {
-            return x
-        }
+    if (x%5 ===0 && x%3 ===0){
+        return "fizzbuzz";
     }
+    else if (x%3 ===0 && x %5 !==0){
+        return "fizz";
+    }
+    else if (x%5 ===0 && x%3 !==0){
+        return "buzz";
+    }
+    else {
+        return x
+    }
+}
 };
 
 module.exports = { fizzbuzz: fizzbuzz };
